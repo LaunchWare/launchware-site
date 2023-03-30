6aa1bb79b99eddaa0862fb300d4d079fe9fb025c
Dan Pickett <dpickett@users.noreply.github.com>
2023-03-29 20:33:36 -0400

    various plugins and config

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+export default {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      backgroundColor: {
+        white: "#ffffff",
+      },
+      textColor: {
+        primary: "#2ccbc0",
+        secondary: "#6930c3",
+      },
+      borderColor: {
+        primary: "#2ccbc0",
+        secondary: "#6930c3",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
