dfa565d231ccdc0dfa48cf822b34267497d3e0a1
Dan Pickett <dpickett@users.noreply.github.com>
2023-04-05 22:57:11 -0400

    list articles on front page for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
 import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import { HeadFC, PageProps, graphql } from "gatsby"
 
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage = (pageProps: PageProps<Queries.MarkdownArticlesForIndexQuery>) => {
+  const { data } = pageProps
+  const listItems = data.allMarkdownRemark.nodes.map(article => {
+    return <li key={article.id}><a href={`/blog${article.fields?.slug}`}>{article.frontmatter?.title}</a></li>
+  })
   return (
     <main>
       <h1>LaunchWare</h1>
+      {listItems && (<ol>{listItems}</ol>)}
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>LaunchWare - Software Development Consulting</title>
+
+export const pageQuery = graphql`
+  query MarkdownArticlesForIndex {
+    allMarkdownRemark(
+      sort: { frontmatter: { publishedAt: DESC } }
+      limit: 1000
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`
