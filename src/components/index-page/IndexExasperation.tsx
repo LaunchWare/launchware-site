eeccf64778c379509f7f011e22fea65b8d7c56da
Martin Serna <martin.serna@gmail.com>
2023-09-14 18:13:15 -0400

    add first two boxes of content

---
+++ src/components/index-page/IndexExasperation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import decorativeCircle from "../../assets/images/decorative-circle.png"
+import "./css/index-exasperation.css"
+
+export const IndexExasperation = () => {
+  return (
+    <div className="index-exasperation">
+      <div className="index-exasperation__contents">
+        <img className="index-exasperation__decorative-circle" src={decorativeCircle} />
+        <div className="index-exasperation__image-container">
+          <img className="index-exasperation__image" src="https://placehold.co/680x520" />
+        </div>
+        <div className="index-exasperation__text-container">
+          <h2 className="index-exasperation__heading">
+            Creating custom software should be exhilarating, not exasperating
+          </h2>
+          <p className="index-exasperation__text">
+            Turning your vision into reality can be overwhelming, and partnering with the wrong software development team can leave you feeling stressed out and frustrated. What’s worse, you could invest a massive amount of time and money without a working product to show for it at the end of it all.
+          </p>
+          <p className="index-exasperation__text">
+            When it comes to your software, the stakes are too high.  Your project should have its chance to make lives better. That's why our custom development team collaborates
+            with our clients to make sure their software solves the
+            right problems and that they build the right products.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { LaunchWareLogo } from "../components/layout/LaunchWareLogo";
 import { PageProps } from "gatsby";
 
-import "../css/index.css";
+import { IndexExasperation } from "../components/index-page/IndexExasperation";
+import { IndexHero } from "../components/index-page/IndexHero";
+import { LaunchWareLogo } from "../components/layout/LaunchWareLogo";
 import { Layout } from "../components/layout/Layout";
+import "../css/index.css";
 
 const IndexPage = (pageProps: PageProps) => {
   return (
     <Layout location={pageProps.location} title="Home">
-      <div className="coming-soon-notice">
-        <div className="content-container">
-          <LaunchWareLogo />
-          <h1>Coming Soon</h1>
-          <a href="/blog">Archive Blog Content</a>
+        <IndexHero />
+        <IndexExasperation />
+        <div className="coming-soon-notice">
+          <div className="content-container">
+            <LaunchWareLogo />
+            <h1>Coming Soon</h1>
+            <a href="/blog">Archive Blog Content</a>
+          </div>
         </div>
-      </div>
     </Layout>
   );
 };
