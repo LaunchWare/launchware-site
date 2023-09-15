856486854d4142cbd773af770acb1203bd1b0966
Dan Pickett <dpickett@users.noreply.github.com>
2023-09-15 19:48:16 -0400

    sc 6803 responsive footer (#5)
    * wip of footer
    
    * complete footer

---
+++ src/configuration/companyContactInformation.ts
@@ -0,0 +1,11 @@
+export const companyContactInformation = {
+  phone: "CHANGE NUMBER",
+  email: "hello@launchware.com",
+  mailingAddress: {
+    streetAddress: "71 Summer Street",
+    streetAddress2: "3rd Floor",
+    city: "Boston",
+    state: "MA",
+    zipCode: "02021",
+  },
+};
---
+++ src/configuration/getCompanySocialProfile.ts
@@ -0,0 +1,46 @@
+import {
+  faXTwitter,
+  faFacebook,
+  faYoutube,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+export class SocialProfileNotFoundError extends Error {
+  constructor(network: string) {
+    super(`Social profile not found for network: ${network}`);
+  }
+}
+
+export const companySocialProfiles = {
+  linkedIn: {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com/company/launchware/",
+  },
+  x: {
+    icon: faXTwitter,
+    url: "https://twitter.com/launchware",
+  },
+  facebook: {
+    icon: faFacebook,
+    url: "https://www.facebook.com/LaunchWare/",
+  },
+  youtube: {
+    icon: faYoutube,
+    url: "https://www.youtube.com/@LaunchWareSoftwareDevelopment",
+  },
+  instagram: {
+    icon: faInstagram,
+    url: "https://www.instagram.com/launchware/",
+  },
+} as Record<string, { icon: any; url: string }>;
+
+export const getCompanySocialProfile = (network: string) => {
+  const result = companySocialProfiles[network];
+
+  if (!result) {
+    throw new SocialProfileNotFoundError(network);
+  }
+
+  return result;
+};
