27be208bb7279caf6a550fc30598fe838f1d3d5e
Dan Pickett <dpickett@users.noreply.github.com>
2023-09-15 21:50:53 -0400

    install google tag manager (#6)

---
+++ src/components/layout/TrackingCodes.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import TagManager from 'react-gtm-module'
+
+export const TrackingCodes = () => {
+  const tagManagerArgs = {
+    gtmId: "GTM-WGNPB4RJ"
+  }
+
+  TagManager.initialize(tagManagerArgs)
+
+  return <></>
+}
