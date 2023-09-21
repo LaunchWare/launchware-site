e53e7b94ffb241dde49cb244a18b4ef224d3d227
Martin Serna <martin.serna@gmail.com>
2023-09-21 14:20:15 -0400

    use font awesome, rename files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ import { IndexBuiltForYou } from "../components/index-page/IndexBuiltForYou";
 import { IndexHero } from "../components/index-page/IndexHero";
 import { IndexLaunched } from "../components/index-page/IndexLaunched";
 import { IndexLogos } from "../components/index-page/IndexLogos";
-import { IndexMoreMomentum } from "../components/index-page/IndexMoreMomentum";
 import { IndexPartnership } from "../components/index-page/IndexPartnership";
 import { IndexShipFaster } from "../components/index-page/IndexShipFaster";
 import { IndexStopStressing } from "../components/index-page/IndexStopStressing";
+import { MoreMomentum } from "../components/index-page/MoreMomentum";
 import { Layout } from "../components/layout/Layout";
 import "../css/index.css";
 
@@ -23,7 +23,7 @@ const IndexPage = (pageProps: PageProps) => {
       <IndexLogos />
       <IndexBuiltForYou />
       <IndexShipFaster />
-      <IndexMoreMomentum />
+      <MoreMomentum />
       <IndexStopStressing />
     </Layout>
   );
