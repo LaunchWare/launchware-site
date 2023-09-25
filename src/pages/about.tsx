21f47351bfc5a1f7c0f7a528ffc27cdd273aea02
Martin Serna <martin.serna@gmail.com>
2023-09-25 15:37:09 -0400

    start About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { PageProps } from "gatsby";
+
+import { AboutHero } from "../components/about-page/AboutHero";
+import { SoftwareExistsTo } from "../components/about-page/SoftwareExistsTo";
+import { Layout } from "../components/layout/Layout";
+import "../css/index.css";
+
+const AboutPage = (pageProps: PageProps) => {
+  return (
+    <Layout location={pageProps.location} title="About">
+      <AboutHero />
+      <SoftwareExistsTo />
+    </Layout>
+  );
+};
+
+export default AboutPage;
