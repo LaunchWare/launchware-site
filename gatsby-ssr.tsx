c2867459b2c28964f40190de28840247455ffb58
Dan Pickett <dpickett@users.noreply.github.com>
2023-10-03 22:32:26 -0400

    lighthouse optimizations

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -11,23 +11,23 @@ const fonts = [
   "/fonts/Kanit/Kanit-ExtraLight.ttf",
   "/fonts/Kanit/Kanit-ExtraLightItalic.ttf",
   "/fonts/Kanit/Kanit-Italic.ttf",
-  "/fonts/Kanit/Kanit-Light.tt",
+  // "/fonts/Kanit/Kanit-Light.ttf",
   "/fonts/Kanit/Kanit-LightItalic.ttf",
   "/fonts/Kanit/Kanit-Medium.ttf",
   "/fonts/Kanit/Kanit-MediumItalic.ttf",
   "/fonts/Kanit/Kanit-SemiBold.ttf",
   "/fonts/Kanit/Kanit-SemiBoldItalic.ttf",
-  "/fonts/Kanit/Kanit-Thin.ttf",
-  "/fonts/Kanit/Kanit-ThinItalic.ttf",
+  // "/fonts/Kanit/Kanit-Thin.ttf",
+  // "/fonts/Kanit/Kanit-ThinItalic.ttf",
   "/fonts/Inter/Inter-SemiBold.ttf",
-  "/fonts/Inter/Inter-Black.ttf",
+  // "/fonts/Inter/Inter-Black.ttf",
   "/fonts/Inter/Inter-Bold.ttf",
-  "/fonts/Inter/Inter-ExtraBold.ttf",
-  "/fonts/Inter/Inter-ExtraLight.ttf",
-  "/fonts/Inter/Inter-Light.ttf",
-  "/fonts/Inter/Inter-Medium.ttf",
+  // "/fonts/Inter/Inter-ExtraBold.ttf",
+  // "/fonts/Inter/Inter-ExtraLight.ttf",
+  // "/fonts/Inter/Inter-Light.ttf",
+  // "/fonts/Inter/Inter-Medium.ttf",
   "/fonts/Inter/Inter-Regular.ttf",
-  "/fonts/Inter/Inter-Thin.ttf",
+  // "/fonts/Inter/Inter-Thin.ttf",
 ]
 export const onRenderBody = ({ setHeadComponents }) => {
   const links = fonts.map((font) => {
@@ -35,19 +35,9 @@ export const onRenderBody = ({ setHeadComponents }) => {
       rel="preload"
       href={font}
       as="font"
-      type="font/ttf"
       crossOrigin="anonymous"
       key={font}
     />
   })
-  setHeadComponents([
-    <link
-      rel="preload"
-      href="/fonts/Inter-roman.var.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-      key="interFont"
-    />,
-  ])
+  setHeadComponents(links)
 }
