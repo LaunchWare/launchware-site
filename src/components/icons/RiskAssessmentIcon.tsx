e78c7927c7b4ccaf3012501a25a01ffa9f97a9d7
Dan Pickett <dan.pickett@launchacademy.com>
2023-10-09 17:25:36 -0400

    make svg more react friendly

--- src/components/icons/RiskAssessmentIcon.tsx
+++ src/components/icons/RiskAssessmentIcon.tsx
@@ -18,14 +18,14 @@ export const RiskAssessmentIcon = ({
       <defs>
         <style>
           .cls-1{"fill:#fff;"}.cls-2,.cls-3{"fill:none;"}.cls-2,.cls-3,.cls-4
-          {"stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;"}.cls-2,.cls-4
+          {"strokeLinecap:round;strokeLinejoin:round;strokeWidth:2px;"}.cls-2,.cls-4
           {"stroke:#000;"}.cls-3{"stroke:#fff;"}
         </style>
       </defs>
       <g>
         <g>
           <rect
-            class="cls-2"
+            className="cls-2"
             x="17.621"
             y="25.135"
             width="54.286"
@@ -34,28 +34,28 @@ export const RiskAssessmentIcon = ({
             ry="6.215"
           />
           <path
-            class="cls-4"
+            className="cls-4"
             d="m50.564,21.277c0-3.203-2.6-5.796-5.802-5.796s-5.802,2.594-5.802,5.796h-6.52v7.869h24.651v-7.869h-6.526Z"
           />
         </g>
-        <polyline class="cls-2" points="26.945 42.638 29.348 45.041 35.049 39.34" />
-        <line class="cls-2" x1="40.871" y1="42.459" x2="61.288" y2="42.459" />
-        <polyline class="cls-2" points="26.945 53.829 29.348 56.232 35.049 50.531" />
-        <line class="cls-2" x1="40.871" y1="54.704" x2="61.288" y2="54.704" />
-        <line class="cls-2" x1="40.871" y1="66.95" x2="55.888" y2="66.95" />
+        <polyline className="cls-2" points="26.945 42.638 29.348 45.041 35.049 39.34" />
+        <line className="cls-2" x1="40.871" y1="42.459" x2="61.288" y2="42.459" />
+        <polyline className="cls-2" points="26.945 53.829 29.348 56.232 35.049 50.531" />
+        <line className="cls-2" x1="40.871" y1="54.704" x2="61.288" y2="54.704" />
+        <line className="cls-2" x1="40.871" y1="66.95" x2="55.888" y2="66.95" />
         <g>
-          <line class="cls-2" x1="27.971" y1="63.924" x2="34.023" y2="69.976" />
-          <line class="cls-2" x1="34.023" y1="63.924" x2="27.971" y2="69.976" />
+          <line className="cls-2" x1="27.971" y1="63.924" x2="34.023" y2="69.976" />
+          <line className="cls-2" x1="34.023" y1="63.924" x2="27.971" y2="69.976" />
         </g>
       </g>
       <g>
         <path
-          class="cls-4"
+          className="cls-4"
           d="m67.737,64.584l-9.65,16.714c-1.853,3.21.463,7.222,4.17,7.222h19.299c3.706,0,6.023-4.012,4.17-7.222l-9.65-16.714c-1.853-3.21-6.486-3.21-8.339,0Z"
         />
         <g>
-          <line class="cls-3" x1="71.907" y1="70.809" x2="71.907" y2="76.687" />
-          <circle class="cls-1" cx="71.907" cy="81.171" r="1.921" />
+          <line className="cls-3" x1="71.907" y1="70.809" x2="71.907" y2="76.687" />
+          <circle className="cls-1" cx="71.907" cy="81.171" r="1.921" />
         </g>
       </g>
     </svg>
--- src/components/icons/SendMessagesIcon.tsx
+++ src/components/icons/SendMessagesIcon.tsx
@@ -27,76 +27,76 @@ export const SendMessagesIcon = ({
       <path
         d="M87.1 31.6V82C87.1 85.3 84.4 88 81.1 88H55.1C51.8 88 49.1 85.3 49.1 82V43V22C49.1 18.7 51.8 16 55.1 16H81.1C84.4 16 87.1 18.7 87.1 22V22.3L62.1 29.1L74.8 35.4"
         stroke="black"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <path
         d="M61.1 55H75.1C78.4 55 81.1 57.7 81.1 61V63C81.1 66.3 78.4 69 75.1 69H61.1C57.8 69 55.1 66.3 55.1 63V61C55.1 57.7 57.7 55 61.1 55Z"
         stroke="black"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <path
         d="M87.1 22.3L92.1 21"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M92.1 21L87.1 31.6L78.8 49.1L74.8 35.5L87.1 25.2L92.1 21Z"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M35 77.5C38.4 77.5 41.1 74.8 41.1 71.5V49C41.1 45.7 43.8 43 47.1 43H49.1"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M32.8 57C34.2 55 35 52.6 35 50C35 43.4 29.6 38 23 38C16.4 38 11 43.4 11 50C11 52.6 11.8 55 13.2 57"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M33.5 70C34.5 68.2 35.1 66.2 35.1 64C35.1 61.4 34.3 59 32.9 57C30.7 54 27.2 52 23.1 52C19 52 15.5 54 13.3 57C11.9 59 11.1 61.4 11.1 64C11.1 66.2 11.7 68.2 12.7 70"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M12.7 70C14.8 66.4 18.7 64 23.1 64C27.5 64 31.4 66.4 33.5 70C34.5 71.8 35.1 73.8 35.1 76C35.1 76.5 35.1 77 35 77.5C34.6 80.5 33.2 83.1 31 85C28.9 86.9 26.1 88 23.1 88C20.1 88 17.3 86.9 15.2 85C12.7 82.8 11.1 79.6 11.1 76C11.1 73.8 11.6 71.8 12.7 70Z"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M15.1 85C15.6 81.1 18.9 78 22.9 78H23H23.1C27.1 78 30.5 81.1 30.9 85"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M23 78C20.8 78 19.1 76.2 19.1 74C19.1 71.8 20.9 70 23.1 70C25.3 70 27.1 71.8 27.1 74C27.1 76.2 25.4 78 23.2 78C23.1 78 23.1 78 23 78Z"
         stroke="black"
-        stroke-width="2"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
