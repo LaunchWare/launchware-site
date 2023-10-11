d1b29697ab3596dbb17f4885e9afb40a648e812d
Dan Pickett <dan.pickett@launchacademy.com>
2023-10-11 12:27:30 -0400

    fix lint issues

--- src/pages/cookies.tsx
+++ src/pages/cookies.tsx
@@ -1,12 +1,14 @@
-import React from "react"
-import { Policy } from "../components/general/Policy"
+import React from "react";
+import { Policy } from "../components/general/Policy";
 import { PageProps } from "gatsby";
 import { Layout } from "../components/layout/Layout";
 
 const PrivacyPolicy = (pageProps: PageProps) => {
-  return <Layout location={pageProps.location} title="Privacy Policy">
-    <Policy policyKey="VTJWVGJXWlNkbkpIT1VoemQxRTlQUT09" />
-  </Layout >
-}
+  return (
+    <Layout location={pageProps.location} title="Privacy Policy">
+      <Policy policyKey="VTJWVGJXWlNkbkpIT1VoemQxRTlQUT09" />
+    </Layout>
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
