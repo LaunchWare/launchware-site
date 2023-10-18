fd16db806d1e3e3c303d112e7bec8a1e4dc57d34
Martin Serna <martin.serna@gmail.com>
2023-10-18 18:59:56 -0400

    show opt in modal for scorecard

---
+++ src/components/scorecard/hooks/useScorecardOptIn.tsx
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useApiClient } from "../../../api-client/hooks/useApiClient";
+import { ScorecardOptInSchema } from "../models/ScorecardOptInSchema";
+
+export const useScorecardOptIn = () => {
+  const client = useApiClient();
+
+  return useMutation(
+    async (data: ScorecardOptInSchema) => {
+      const resp = await client.post(`/scorecard`, data, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+      return resp.data;
+    },
+    {
+      onError: (err: Error | AxiosError) => {
+        console.error(err);
+      },
+    },
+  );
+};
