4298cb6108d93718a4aa6125f3724838196e461a
Dan Pickett <dan.pickett@launchacademy.com>
2023-11-10 12:57:03 -0500

    debugging netlify function

--- functions/submission-created.ts
+++ functions/submission-created.ts
@@ -6,8 +6,11 @@ import { ScorecardOptInSchema } from "../src/components/contact-page/models/Scor
 export const handler: Handler = async function (event, context) {
   await configure();
   const body = JSON.parse(event.body || "")
+  console.log(body)
   const result = body.payload.data
-  if (result["form-name"] === "scorecardOptIn") {
+  console.log(result["form-name"])
+  if (body?.payload["form_name"] === "scorecardOptIn") {
+    console.log("attempting to save")
     const event = new Event<ScorecardOptInSchema>({
       email: result.email,
       firstName: result.firstName,
