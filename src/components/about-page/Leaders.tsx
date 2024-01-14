b1c65ab1bef25a3c0978603aef2bce66298b0c3c
Dan Pickett <dan.pickett@launchware.com>
2024-01-14 16:13:46 -0500

    remove Bill, fix Dan linkedIn

--- src/components/about-page/Leaders.tsx
+++ src/components/about-page/Leaders.tsx
@@ -14,7 +14,7 @@ export const Leaders = () => {
       {modal}
       <div className="leaders__contents">
         <div className="leaders__heading-container">
-          <h2 className="leaders__heading">Meet LaunchWare's Leaders</h2>
+          <h2 className="leaders__heading">Meet LaunchWare's Founder</h2>
           <p></p>
         </div>
         <div className="leaders__grid">
@@ -67,65 +67,7 @@ export const Leaders = () => {
               </p>
               <ul className="leaders__social-icons">
                 <li>
-                  <a href="https://www.linkedin.com/in/danpickett">
-                    <FontAwesomeIcon icon={faLinkedin} />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="leaders__card">
-            <div className="leaders__image-container">
-              <StaticImage
-                aspectRatio={2.5}
-                quality={90}
-                transformOptions={{ fit: "cover", cropFocus: "north" }}
-                className="leaders__image"
-                src="../../images/leader-headshots/bill-rowell.jpeg"
-                alt="Bill Rowell Headshot"
-                width={604}
-                height={400}
-              />
-            </div>
-            <div className="leaders__text-container">
-              <h3 className="leaders__card-heading">Bill Rowell</h3>
-              <h3 className="leaders__card-heading__italic">Principal Engineer</h3>
-              <p className="leaders__text">
-                In an industry where technology often takes the center stage, Bill understands the
-                importance of the human element in software development. It’s critical. In Bill's
-                view, a healthy dialogue between every party involved in a project is not just
-                beneficial but essential for the timely delivery of valuable software solutions.
-              </p>
-              <p className="leaders__text">
-                Bill's empathy stems from a simple yet profound belief: Every participant in a
-                project is an individual human being. We all have our personal and professional
-                hurdles, and we all make mistakes. By keeping the people, both developers and
-                end-users, at the forefront, he champions the creation of long-term, effective
-                solutions.
-              </p>
-              <p className="leaders__text">
-                With over 20 years as a Software Engineer, Bill brings a wealth of experience to the
-                table. He has thrived in various team sizes and has also managed departments of 30
-                or more, enabling him to address a spectrum of challenges. He’s an authority on both
-                the technical and business nuances of software development.
-              </p>
-              <p className="leaders__text">
-                A graduate of WPI in Computer Science, Bill blends his professional pursuits with an
-                enriching personal life in Massachusetts. A loving husband and father of two, he's
-                an avid mountain biker and skier, making the most of New England's diverse seasons.
-                His family time is cherished, whether they're on the road or engaging in domestic
-                projects.
-              </p>
-              <p className="leaders__text">
-                One commendation from a previous manager succinctly captures Bill's unique value:
-                “He thinks more strategically about our business, offering a refreshing perspective
-                amid operation-focused managers.” For Bill, the business impact of his role is just
-                as fulfilling as its technical aspects. He’s a well-rounded leader who knows how to
-                balance human considerations with business imperatives.
-              </p>
-              <ul className="leaders__social-icons">
-                <li>
-                  <a href="https://www.linkedin.com/in/billr578">
+                  <a href="https://www.linkedin.com/in/dpickett">
                     <FontAwesomeIcon icon={faLinkedin} />
                   </a>
                 </li>
