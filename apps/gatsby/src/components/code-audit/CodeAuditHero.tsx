49619d0f6211d1b3c4eb860199934ade18443bae
Casi Newell <19928431+CassandraNewell@users.noreply.github.com>
2024-07-30 12:52:35 +0200

    code audit text

---
+++ apps/gatsby/src/components/code-audit/CodeAuditHero.tsx
@@ -0,0 +1,16 @@
+// STATUS: complete
+import React from "react";
+
+import "./css/custom-development-hero.css";
+
+export const CodeAuditHero = () => (
+  <div className="code-audit-hero">
+    <div className="code-audit-hero__contents">
+      <h1 className="code-audit-hero__heading">Turn Code Chaos into Seamless Success</h1>
+      <p className="code-audit-hero__text">
+        Discover the power of expert code audits with LaunchWare. We help you navigate your software
+        challenges and set the stage for success.
+      </p>
+    </div>
+  </div>
+);
