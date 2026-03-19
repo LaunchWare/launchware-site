89c7403810b5b9c854ad13b04b8e4e7b178e03a3
Dan Pickett <dan.pickett@launchware.com>
2026-03-19 13:32:06 -0400

    upgrade dependencies

--- apps/gatsby/postcss.config.js
+++ apps/gatsby/postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "postcss-nesting": {},
-    "@tailwindcss/nesting": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
   },
